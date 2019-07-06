b6bfda607ee0af30dad7b686b452155ae2b98126
Audrey Livirya <aliv074@aucklanduni.ac.nz>
2019-07-06 19:06:39 +1200

    Squashed commit of the following:
    commit 4896dfcf67952e62bac42c1dc46888ffef15ff22
    Author: Audrey Livirya <aliv074@aucklanduni.ac.nz>
    Date:   Fri Jul 5 16:25:23 2019 +1200
    
        Squashed commit of the following:
    
        commit 7ac322c66c448470ca55e91ad6dab662f2d85316
        Author: Unknown <amalchandra97@hotmail.com>
        Date:   Wed Jul 3 12:07:07 2019 +1200
    
            Highscore-Partial-Fix
    
        commit f5683d98e3072105202da2e8f27b2a83de15a2b3
        Author: Audrey Livirya <aliv074@aucklanduni.ac.nz>
        Date:   Sun Jun 30 00:44:24 2019 +1200
    
            Added icon to application, and also changed it so that the application appears after installed
    
        commit dd7615f17891b92a6a0d5db94385654c0cf59979
        Author: Audrey Livirya <aliv074@aucklanduni.ac.nz>
        Date:   Sun Jun 30 00:26:55 2019 +1200
    
            Integrated options dialog with video/quiz. TODO: VIdeo is not specific yet to the option... Also generally generalize variables so they are the same. Needs some refactoring... NOTE, ran into a MASSIVE issue about values not being updated quickly enough, thus actions need to be done after the onsuccesslistener gives a result
    
        commit b196d5b41bd61e536135a9f54d906b429f185889
        Merge: d5604c4 a19a6aa
        Author: Audrey Livirya <aliv074@aucklanduni.ac.nz>
        Date:   Sat Jun 29 15:09:50 2019 +1200
    
            Merge branch 'Youtube-player' into alert-dialog
    
        commit d5604c431a77785e49f21df8b4dfffbe1e0ea2b6
        Author: Audrey Livirya <aliv074@aucklanduni.ac.nz>
        Date:   Sat Jun 29 15:04:35 2019 +1200
    
            Removed dialog from manifest -- probably should rename as well
    
        commit 2760e2b497764f7f3cc556335a13d1c2b151adf4
        Author: Audrey Livirya <aliv074@aucklanduni.ac.nz>
        Date:   Sat Jun 29 14:52:02 2019 +1200
    
            Added alert dialog for choosing options, and modified model files to include all lab equipment that is applicable
    
        commit a19a6aa268500c06eeead418c12742f534d0dbc5
        Author: Unknown <amalchandra97@hotmail.com>
        Date:   Fri Jun 28 14:39:37 2019 +1200
    
            Added quiz
    
        commit ff0564007998401831ab80494cf9f0246052f38b
        Author: Unknown <amalchandra97@hotmail.com>
        Date:   Wed Jun 26 15:20:06 2019 +1200
    
            Add Youtube Player

--- app/src/main/java/com/project/laborientation/MainActivity.java
+++ app/src/main/java/com/project/laborientation/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Toast;
@@ -29,6 +32,13 @@ import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.project.laborientation.Quiz.Category;
+import com.project.laborientation.Quiz.QuizDbHelper;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -38,10 +48,22 @@ public class MainActivity extends AppCompatActivity {
     private LoginButton loginButton;
     private String TAG = "MainActivity";
 
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "sharedPrefs";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewHighscore = findViewById(R.id.text_view_highscore);
+        spinnerCategory = findViewById(R.id.spinner_category);
+        loadCategories();
+        loadHighscore();
+
+        Button button_text = findViewById(R.id.button_text);
         // Initialize Facebook Login button
         mCallbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.fb_login_button);
@@ -71,6 +93,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void loadCategories(){
+        QuizDbHelper dbHelper = QuizDbHelper.getInstance(this);
+        List<Category> categories = dbHelper.getAllCategories();
+
+        ArrayAdapter<Category> adapterCategories = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+        adapterCategories.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(adapterCategories);
+
+    }
+
+
+    private void loadHighscore(){
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highscore = prefs.getInt(KEY_HIGHSCORE, 0);
+        textViewHighscore.setText("HighScore: " + highscore);
+    }
+
+    private void updateHighscore(int highscoreNew){
+        highscore = highscoreNew;
+        textViewHighscore.setText("HighScore: " + highscore);
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highscore);
+        editor.apply();
+
     }
 
 
---
+++ app/src/main/java/com/project/laborientation/Quiz/Category.java
@@ -0,0 +1,40 @@
+package com.project.laborientation.Quiz;
+
+public class Category {
+    public static final int OSCILLOSCOPE = 1;
+    public static final int MULTIMETER = 2;
+    public static final int POWER_SUPPLY = 3;
+    public static final int WAVEFORM_GENERATOR = 4;
+    public static final int PHOEBE = 5;
+
+
+    private int id;
+    private String name;
+
+    public Category(){}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return getName();
+    }
+}
