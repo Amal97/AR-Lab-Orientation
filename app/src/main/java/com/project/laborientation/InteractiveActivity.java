ddcb43b2413a991558c4073c59d5f336e31ca292
Audrey Livirya <aliv074@aucklanduni.ac.nz>
2019-07-13 19:22:08 +1200

    Corroborated touch to item

--- app/src/main/java/com/project/laborientation/InteractiveActivity.java
+++ app/src/main/java/com/project/laborientation/InteractiveActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class InteractiveActivity extends AppCompatActivity {
     ImageView mEquipment;
@@ -32,7 +33,7 @@ public class InteractiveActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 int rgb = getColour(Math.round(motionEvent.getX()), Math.round(motionEvent.getY()));
-                Log.i(TAG, Integer.toHexString(rgb));
+                handleColor(Integer.toHexString(rgb));
                 return false;
             }
         });
@@ -48,6 +49,30 @@ public class InteractiveActivity extends AppCompatActivity {
         canvas.drawColor(Color.WHITE);
         transparentEquipmentDrawable.draw(canvas);
         return bitmap.getPixel(x, y);
+    }
+
+    private void handleColor(String colorHex) {
+        switch (colorHex) {
+            case "ffffaec7":
+                Toast.makeText(InteractiveActivity.this, "Power Button", Toast.LENGTH_LONG).show();
+                break;
+            case "ffb97b56":
+                Toast.makeText(InteractiveActivity.this, "Voltage", Toast.LENGTH_LONG).show();
+                break;
+            case "ffffca18":
+                Toast.makeText(InteractiveActivity.this, "Current", Toast.LENGTH_LONG).show();
+                break;
+            case "ff0ed145":
+                Toast.makeText(InteractiveActivity.this, "Master Voltage", Toast.LENGTH_LONG).show();
+                break;
+            case "ffeb1c24":
+                Toast.makeText(InteractiveActivity.this, "Knobs", Toast.LENGTH_LONG).show();
+                break;
+            case "ff3f47cc":
+                Toast.makeText(InteractiveActivity.this, "Screen", Toast.LENGTH_LONG).show();
+                break;
+        }
+
     }
 
 }
