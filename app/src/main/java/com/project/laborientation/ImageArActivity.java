3752b8121cf51a0e0072747cb432257a7b01b03b
Amal <amalchandra97@hotmail.com>
2019-07-18 13:36:05 +1200

    all activity avaliable

---
+++ app/src/main/java/com/project/laborientation/ImageArActivity.java
@@ -0,0 +1,14 @@
+package com.project.laborientation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageArActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_ar);
+    }
+}
