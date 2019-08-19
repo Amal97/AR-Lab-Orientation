e088ed9bf9dd3affd80062b132fcf110c9098cd9
Audrey Livirya <aliv074@aucklanduni.ac.nz>
2019-08-19 17:09:29 +1200

    Modified interactive so that it should be easy to change

--- app/src/main/java/com/project/laborientation/InteractiveActivity.java
+++ app/src/main/java/com/project/laborientation/InteractiveActivity.java
@@ -3,7 +3,7 @@ package com.project.laborientation;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import android.annotation.SuppressLint;
-import android.content.pm.ActivityInfo;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -19,6 +19,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class InteractiveActivity extends AppCompatActivity {
     ImageView mEquipment;
+    String categoryName;
+    Drawable transparentEquipmentDrawable;
     private static final String TAG = "InteractiveActivity";
 
     @SuppressLint("ClickableViewAccessibility")
@@ -26,12 +28,16 @@ public class InteractiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interactive);
-        mEquipment = findViewById(R.id.initial_image);
+        Intent intent = getIntent();
+        String categoryName = intent.getStringExtra(CameraActivity.EXTRA_CATEGORY_Name).toLowerCase();
+        setInitialImage(categoryName);
+        setDrawableImage(categoryName);
+        mEquipment = findViewById(R.id.object_image);
         mEquipment.setOnTouchListener((View view, MotionEvent motionEvent) -> {
             int positionX = Math.round(motionEvent.getX());
             int positionY = Math.round(motionEvent.getY());
             int rgb = getColour(positionX, positionY);
-            handleColor(Integer.toHexString(rgb));
+            handleColor(Integer.toHexString(rgb), categoryName);
             return false;
         });
     }
@@ -40,7 +46,6 @@ public class InteractiveActivity extends AppCompatActivity {
     {
         Log.d(TAG, "Identifying color");
         LinearLayout mInteractiveLayout = findViewById(R.id.interactive_layout);
-        Drawable transparentEquipmentDrawable = ContextCompat.getDrawable(this, R.drawable.best_transparent);
         transparentEquipmentDrawable.setBounds(0, 0, mInteractiveLayout.getWidth(), mInteractiveLayout.getHeight());
         Bitmap bitmap = Bitmap.createBitmap(mInteractiveLayout.getWidth(), mInteractiveLayout.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
@@ -49,9 +54,75 @@ public class InteractiveActivity extends AppCompatActivity {
         return bitmap.getPixel(x, y);
     }
 
-    private void handleColor(String colorHex) {
+    private void setDrawableImage(String object) {
+        switch (object) {
+            case "oscilloscope":
+                break;
+            case "power supply":
+                transparentEquipmentDrawable =  ContextCompat.getDrawable(this, R.drawable.best_transparent);
+            case "waveform generator":
+                break;
+            case "multimeter":
+                break;
+            case "phoebe":
+                //Do nothing
+                break;
+            default:
+                break;
+        }
+    }
+    private void setInitialImage(String object) {
+        ImageView objectImage = findViewById(R.id.object_image);
+        switch (object) {
+            case "oscilloscope":
+                break;
+            case "power supply":
+                objectImage.setImageResource(R.drawable.best);
+                break;
+            case "waveform generator":
+                break;
+            case "multimeter":
+                break;
+            case "phoebe":
+                //Do nothing
+                break;
+            default:
+                break;
+        }
+    }
+    private void handleColor(String colorHex, String object) {
         LinearLayout interactiveLayout = findViewById(R.id.interactive_layout);
+        switch (object) {
+            case "oscilloscope":
+                handleOscilloscope(colorHex, interactiveLayout);
+                break;
+            case "power supply":
+                handlePowerSupply(colorHex, interactiveLayout);
+                break;
+            case "waveform generator":
+                handleWaveformGenerator(colorHex, interactiveLayout);
+                break;
+            case "multimeter":
+                handleMultimeter(colorHex, interactiveLayout);
+                break;
+            case "phoebe":
+                //Do nothing
+                break;
+                default:
+                    break;
+        }
 
+    }
+
+    private void handleOscilloscope(String colorHex, LinearLayout interactiveLayout) {
+
+    }
+
+    private void handleMultimeter(String colorHex, LinearLayout interactiveLayout) {
+
+    }
+
+    private void handlePowerSupply(String colorHex, LinearLayout interactiveLayout) {
         switch (colorHex) {
             case "ffffaec7":
                 Snackbar.make(interactiveLayout, "Power button", Snackbar.LENGTH_LONG)
@@ -80,6 +151,10 @@ public class InteractiveActivity extends AppCompatActivity {
             default:
                 break;
         }
+    }
+
+    private void handleWaveformGenerator(String colorHex, LinearLayout interactiveLayout) {
+
     }
 
 }
