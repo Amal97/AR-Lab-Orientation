c57b204a84c8c48fdce50bc46813ee597cfce727
Audrey Livirya <aliv074@aucklanduni.ac.nz>
2019-08-30 00:26:38 +1200

    Changed rename file to be more appropriate

--- machine-learning/rename.py
+++ machine-learning/rename.py
@@ -9,16 +9,14 @@ def main():
     for folder in os.listdir("images"): 
         i = 0
         for file in os.listdir("images/" + folder): 
-            if folder not in file:
-                dst = folder + str(i) + ".jpg"
-                src = 'images/'+ folder + '/' + file 
-                dst = 'images/'+ folder + '/' + dst 
-                
-                # rename() function will 
-                # rename all the files 
-                os.rename(src, dst) 
-                i += 1
-        
+            dst = folder + str(i) + ".jpg"
+            src = 'images/'+ folder + '/' + file 
+            dst = 'images/'+ folder + '/' + dst 
+            
+            # rename() function will 
+            # rename all the files 
+            os.rename(src, dst) 
+            i += 1        
     
 
 # Driver Code 
