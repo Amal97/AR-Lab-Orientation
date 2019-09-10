24324a07a46e4b91146be85c6bd0a0bd7bb389c0
Amal <amalchandra97@hotmail.com>
2019-09-10 21:37:19 +1200

    Added manual text search for staff info

---
+++ app/src/main/java/com/project/laborientation/FindLecturer_text_input.java
@@ -0,0 +1,14 @@
+package com.project.laborientation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindLecturer_text_input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_lecturer_text_input);
+    }
+}
